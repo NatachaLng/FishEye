64357a221d6b8765fff099f778767233925580db
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2020-12-23 12:00:26 +0100

    add ressources

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-/*https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON*/
+/*https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON
+
+ p = document.getElementById("para1");
+p_prime = p.cloneNode(true);
+
+https://medium.com/better-programming/javascript-design-patterns-25f0faaaa15
+
+https://refactoring.guru/design-patterns/factory-method
+
+https://www.alsacreations.com/article/lire/1161-json-ajax-jquery-jsonp-getjson.html*/
