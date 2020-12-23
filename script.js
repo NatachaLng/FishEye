257c1ae0c7b7e57d6a025ead7d46ba2a9b360e66
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2020-12-23 15:09:42 +0100

    add ressources

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ https://medium.com/better-programming/javascript-design-patterns-25f0faaaa15
 
 https://refactoring.guru/design-patterns/factory-method
 
-https://www.alsacreations.com/article/lire/1161-json-ajax-jquery-jsonp-getjson.html*/
+https://www.alsacreations.com/article/lire/1161-json-ajax-jquery-jsonp-getjson.html
+
+https://developer.mozilla.org/fr/docs/Learn/JavaScript/Objects/JSON*/
