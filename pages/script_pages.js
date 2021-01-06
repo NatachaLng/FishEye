db16d7a21b1cbc20a96863ff433272562e4559c2
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-01-06 16:30:28 +0100

    creation photographe page

---
+++ pages/script_pages.js
@@ -0,0 +1,138 @@
+/*https://stackoverflow.com/questions/61207082/generate-pages-based-from-a-json-file
+*/
+
+var photographer;
+var photographs;
+var media;
+
+function load(body){
+    photographs=body['photographers'];
+    media=body['media'];
+}
+
+function filter() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  //like function
+  window.onload = function(){
+	let likeBtn = document.getElementById('like__btn');
+	let numberLike = parseInt(document.getElementById('number__likes').textContent); 
+	if(likeBtn){
+		likeBtn.onclick = function(){
+			numberLike += 1;
+			document.getElementById('number__likes').textContent = numberLike; //Attention, IE n'aime pas textContent. Il faut voir avec innerText
+			return false;
+		}
+	}
+}
+
+//modal 
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelector(".contact__btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+
+// launch modal event
+modalBtn.addEventListener("click", launchModal);
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Form elements
+let firstName = document.getElementById("first");
+let msgFirst = document.getElementById("msgFirst");
+let msgLast = document.getElementById("msgLast");
+let lastName = document.getElementById("last");
+let email = document.getElementById("email");
+let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let message = document.getElementById("message");
+let msgMessage = document.getElementById("msgMsg");
+
+//validate first name
+function validateFirstName(){
+    if ((firstName.value.length < 2) || (!firstName.value.match(/^([a-zA-Z]+)$/))){ //double check from HTML
+      msgFirst.style.display = "block";
+    return false;
+      }
+      msgFirst.style.display = "none";
+    return true;
+  }
+
+//validate last name
+function validateLastName(){
+    if ((lastName.value.length < 2) || (!lastName.value.match(/^([a-zA-Z]+)$/))){ //double check from HTML
+      msgLast.style.display = "block";
+    return false;
+      }
+      msgLast.style.display = "none";
+    return true;
+  }
+  
+  //validation email 
+  function validateEmail(){
+    if (!email.value.match(mailformat)){ //double check from HTML
+    return false;
+      }
+    return true;
+  }
+
+  //validate message
+function validateMessage(){
+    if (message.value.length < 2){ //double check from HTML
+      msgMessage.style.display = "block";
+    return false;
+      }
+      msgMessage.style.display = "none";
+    return true;
+  }
+
+  function validate(){
+    formValid = true;
+    formValid=(formValid && validateFirstName());
+    console.log(firstName.value);
+    formValid=(formValid && validateLastName());
+    console.log(lastName.value);
+    formValid=(formValid && validateEmail());
+    console.log(email.value); 
+    formValid=(formValid && validateMessage());
+    console.log(message.value);
+    return formValid; // return true as formValid=true
+  }
+
+//display confirmation
+let formSent = document.getElementById("form__sent"); //validation message
+
+if(window.location.href.indexOf("?") > 1){ //if the url contain a ? (because of the get method of the datas in the form)
+  formSent.style.display = "block"; //then we display the validation message
+  }
+else{
+  formSent.style.display="none"; //otherwise we don't 
+}
+
+
+fetch("https://natachalng.github.io/NatachaLang_6_21122020/data/FishEyeDataFR.json").then (data => data.json().then (json => load(json)));
--- script.js
+++ script.js
@@ -39,11 +39,10 @@ function load(body){
     media=body['media'];
     createCards();
     createTaglist();
+    loadCheckboxes();
 }
 
-/*function index(){
 
-}*/
 let allTag = [];
 function addTags(photographTag){
     for (let h=0; h<photographs.length; h++){
@@ -56,6 +55,7 @@ function addTags(photographTag){
     }
 }   
 
+// creation of the tag list
 function createTaglist (){
     addTags();
     for (let k = 0; k < allTag.length; k++){
@@ -76,10 +76,41 @@ function createTaglist (){
     }
 }
 
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+const cardContainer = document.getElementById("photographer");
+let checkboxValues = [ ]; 
+
+console.log(checkboxes);
+
+function loadCheckboxes(){
+checkboxes.forEach((box) => {
+    box.checked = false;
+    box.addEventListener("change", () => filterCards());
+});}
+
+function grabCheckboxValues() {
+    let checkboxValues = [];
+    checkboxes.forEach((checkbox) => {
+          if (checkbox.checked) checkboxValues.push(checkbox.value);
+    });
+    return checkboxValues;
+}
+
+function filterCards() {
+    console.log("check");
+    cardContainer.innerHTML = "";
+    checkboxValues = grabCheckboxValues();data.forEach((item) => {
+          let classes = item.classes;
+          let result = (arr, target) => target.every((v) => arr.includes(v));let isMatch = result(classes, checkboxValues);
+          if (isMatch) {
+                cardContainer.innerHTML += article;
+          }
+    });}
+
 /*let alltags=document.querySelectorAll('.checkbox').forEach(tags =>{
     if (tags.checked == true){
         console.log('checked');
-        let card = document.getElementByClassName('card')
+        let card = document.getElementByClassName('card');
         card.style.display='none';
         let filterValue = tags.value;
         let filtered = element.classList.contains(filterValue);
@@ -87,10 +118,12 @@ function createTaglist (){
     }
 });*/
 
+// creation of the cards
+let article 
 function createCards(photograph) {
     for (let i = 0; i < photographs.length; i++) {
         photograph = photographs[i];
-            let article = `<article id="${photograph.id}" class="card">
+            let article = `<article data-aos="fade-up" data-aos-duration=100 id="${photograph.id}" class="card">
             <img class="card__image" src="${photograph.chosenPicture}" alt="picture ${photograph.name}">
             <h3 class="card__name">${photograph.name}</h3>
             <p class="card__location">${photograph.city}, ${photograph.country}</p>
@@ -102,6 +135,18 @@ function createCards(photograph) {
         </article>`;
         document.getElementById('photographer').innerHTML += article;
     }
+}
+
+function pagePhotographer (){
+    for (let i = 0; i < photographs.length; i++) {
+    photograph = photographs[i];
+    let newPage = window.open();
+    let currentURL = window.location.href;
+    let url = new URL(currentURL);
+    url.searchParams.append('id', photographs[i].id);
+    let html = '<html><head> <script src="pages/script_pages.js"></script> <link rel="stylesheet" type="text/css" href="pages/stylesheets/style_pages.css"></head>    <body></body></html>';
+    newPage.document.write(html);
+}
 }
 
 fetch("https://natachalng.github.io/NatachaLang_6_21122020/data/FishEyeDataFR.json").then (data => data.json().then (json => load(json)));
