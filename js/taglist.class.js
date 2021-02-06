53861c170f39dec88a56e8bff6a1cbae2e088a89
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-02-06 16:07:33 +0100

    Taglist creation

--- js/taglist.class.js
+++ js/taglist.class.js
@@ -11,22 +11,37 @@ class TagList {
         init()
         {
             console.log('init()');
-            this.createTaglist();
+            this.allTaglist();
+            this.cleanTagList();
+            this.createTagList();
 
             // @todo: onclick
         }
 
-        createTaglist()
-        {
-            console.log("CreateTagList");
-            console.log(this.db);
-            for (let p of this.db.getDatas().photographers) {
-                let tags = new tag(
-                    p.tags
-                );
 
+        allTaglist()
+        {
+            let allTags = new Array();
+            for (let p of this.db.getDatas().media) {
+                let mediaTags = new tag(
+                    p.tags[0])
+                    allTags.push(mediaTags);
                 // Add to List
-                document.querySelector(this.selector_id_list).innerHTML += tags.getTagHTML();
+                //document.querySelector(this.selector_id_list).innerHTML += mediaTags.getTagHTML();
             }
+            return allTags;
+        }
+
+        cleanTagList(){
+           let allTags = this.allTaglist();
+            const uniqueTag = Array.from(new Set(allTags.map(a => a.tags)))
+                .map(tags => {
+                    return allTags.find(a => a.tags === tags)
+                })
+           return uniqueTag;
+        }
+
+        createTagList(){
+
         }
     }
